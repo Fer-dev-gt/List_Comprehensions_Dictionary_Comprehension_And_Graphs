3e2d53aa95224cecfa4b27a3267cfc76e2ef32b8
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-01-23 23:43:33 -0600

    Mejoras de legibilidad y mejor explicacion sobre Dictionary Comprehensions con .csv files

--- read_csv.py
+++ read_csv.py
@@ -1,42 +1,33 @@
-# Para limpiar la shell cuando se ejecute
-import os 
+import os                                               # Para limpiar la shell cuando se ejecute
+import csv                                              # Primero importamos un modulo nativo que tiene Python para leer archivos CSV
 os.system('clear')
 
-# Primero importamos un modulo nativo que tiene Python para leer archivo CSV
-import csv
+"""                                                     # Mostramos los datos fila a fila                                        
+                                                        # Creamos una funcion que se ejecuta con if __name__... y usamos "with open()"" ademas de funciones del modulo csv                                  
+  def read_csv(path):                                   # El parametro "path" es donde esta ubicado nuestro .csv 
+    with open(path, 'r') as csvfile:                    # Usamos funcion "with open()" y le damos un alias "csvfile"
+      reader = csv.reader(csvfile, delimiter = ',')     # Creamos un "reader" dandole como parametro el alias y usamos un delimitador ',' dependiento de como este en el .csv   
+      for row in reader:                                # Por cada fila en el "reader"
+        print('***' * 12)
+        print(row)                                      # Mostramos los datos como de cada fila y los muestra como una lista"""  
 
-# De esta manera mostramos los datos fila a fila y usamos un delimitador = ',' porque asi viene la fuente de datos
-
-# Tambien creamos una funcion que se ejecuto con if __name__... y usamos with open() y funciones del modulo csv
-
-"""def read_csv(path):
+    
+def read_csv(path):                                     # Mostramos los datos como una lista de diccionarios, donde la llave es el nombre de la columna para mostrar los valores
   with open(path, 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter = ',')
-    for row in reader:
-      print('***' * 12)
-      print(row)"""
-# Los datos me los muestra como una lista []
-      
+    reader = csv.reader(csvfile, delimiter = ',')       # Creamos un "Reader"
+    header = next(reader)                               # Guardamos los nombres de las columnas
+    data = []                                           # Creamos una lista vacia
 
-# Aca mostramos los datos como una lista de diccionarios, donde la llave es el nombre de la columna para mostrar los valores
-def read_csv(path):
-  with open(path, 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter = ',')
-    header = next(reader) # Aqui guardamos los nombres de las columnas
-    data = []
     for row in reader:
-      iterable = zip(header, row)
-      """Imprime listas de pares llave-valor en forma de tupla
-      print(list(iterable))"""
+      iterable = zip(header, row)                                         # Usando la funcion "zip()" creamos un diccionario uniendo el nombre de las columnas (header) con todas las filas (row)
+      #print(list(iterable))                                              # Imprime listas de pares llave-valor en forma de tupla
       
-      # Aca lo convertimos a un formato de diccionarios con "dictionary comprehension"
-      country_dictionary = {key : value for key, value in iterable}
-      data.append(country_dictionary)
+      country_dictionary = {key : value for key, value in iterable}       # Convertimos a un formato diccionario usando "dictionary comprehension" usando "iterable" 
+      data.append(country_dictionary)                                     # A la lista vacia "data" le agregamos al final todos los valores de las filas con los nombres de las columnas
       
-    return data
+    return data                                                           # Retornamos la lista con el formato deseado (lista de Diccionarios)
       
   
-# Con esto nos aseguramos que el programa se ejecute como un script desde la terminal
-if __name__ == '__main__':
-  data = read_csv('./app/data.csv')
-  print(data[0])
+if __name__ == '__main__':                                                # Con esto nos aseguramos que el programa se ejecute como un script desde la terminal
+  data = read_csv('./data.csv') 
+  print(data[0:9])                                                        # Imprimimos el pais o paises (valor) deseados de la lista de paises
